bc8fad54e7e5f1de6b3e24e1bd3d72d83ca43e7f
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-02 20:00:17 +0700

    added tabs to story and franchises

--- franchise.php
+++ franchise.php
@@ -50,29 +50,26 @@
     </div>
   </nav>
   <div class="container-fluid"id="container">
-    <div class="row align-items-start"></div>
     <div class="row">
-      <div class="col-9"></div>
-      <div class="col-6 col-xl-3">
-        <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th scope="col">Information</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row"><a href="">Sum</a></th>
-              </tr>
-              <tr>
-                <th scope="row"><a href="">Genre</a></th>
-              </tr>
-              <tr>
-                <th scope="row"><a href="">Tags</a></th>
-              </tr>
-            </tbody>
-          </table>
+      <div class="col-xl-10">
+        <div class="row">
+          <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
+            <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
+            <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
+            <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+          </div>
+        </div>
       </div>
+      <div class="col-xl-2">
+        <div class="list-group" id="list-tab" role="tablist">
+          <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+          <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+          <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+          <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 </body>
