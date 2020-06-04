c1769b69c5ac8955c9cbd142f39036f751bdca0b
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-04 20:13:56 +0700

    removed stylesheet link in franchise page

--- franchise.php
+++ franchise.php
@@ -11,7 +11,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Franchise - ReadHere</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <nav class="navbar navbar-expand-xl navbar-light bg-light navbar-fixed-top"  id="navbar">
