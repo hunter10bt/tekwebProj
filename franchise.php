74f9c4f43ebf494e633e481105dcdb89648c081e
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-09 10:07:01 +0700

    franchise page now lists stories

--- franchise.php
+++ franchise.php
@@ -89,13 +89,53 @@
             <div class="tab-pane fade" id="list-story" role="tabpanel" aria-labelledby="list-story-list">
               <h2>List of Stories</h2>
               <div class="list-group" id="story-list">
-                <a href="#" class="list-group-item list-group-item-action">Item</a>
+                <?php
+                  $query = "SELECT storyID,title,author,summary FROM story WHERE readable = 1 AND storyID=any(SELECT storyID FROM tagdetails WHERE franchiseID = '$franchiseID') ORDER BY storyID desc";
+                  $storyRes = mysqli_query($con, $query);
+
+                  if ($storyRes) {
+                    while($row = mysqli_fetch_array($storyRes)){
+                      echo "
+                      <a href='story.php?id=$row[0]' class='list-group-item list-group-item-action'>
+                        <div class='d-flex w-100 justify-content-between'>
+                          <h4 class='mb-1'>$row[1]</h5>
+                          <!--<small>3 days ago</small>-->
+                        </div>
+                        <p class='mb-1'>$row[3]</p>
+                        <small>By $row[2]</small>
+                      </a>";
+                    }
+                  }
+                  else {
+                    echo "There are currently no stories for this franchise";
+                  }
+                ?>
               </div>
             </div>
             <div class="tab-pane fade" id="list-discussions" role="tabpanel" aria-labelledby="list-discussions-list">
               <h2>List of Discussions</h2>
               <div class="list-group" id="discussion-list">
-                <a href="#" class="list-group-item list-group-item-action">Item</a>
+              <?php
+                  $query = "SELECT discussionID,title,user,summary FROM discussion WHERE readable = 1 AND franchiseID = '$franchiseID'";
+                  $storyRes = mysqli_query($con, $query);
+
+                  if ($storyRes) {
+                    while($row = mysqli_fetch_array($storyRes)){
+                      echo "
+                      <a href='story.php?id=$row[0]' class='list-group-item list-group-item-action'>
+                        <div class='d-flex w-100 justify-content-between'>
+                          <h4 class='mb-1'>$row[1]</h5>
+                          <!--<small>3 days ago</small>-->
+                        </div>
+                        <p class='mb-1'>$row[3]</p>
+                        <small>By $row[2]</small>
+                      </a>";
+                    }
+                  }
+                  else {
+                    echo "No discussions.";
+                  }
+                ?>
               </div>
             </div>
           </div>
