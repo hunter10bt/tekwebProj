6b0b12355c5951f4422ade4ca4606ad3fe6b5043
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-11 20:52:36 +0700

    adding new chapter function - in progress

--- addChapter.php
+++ addChapter.php
@@ -1,16 +1,36 @@
 <?php
   session_start();
   include "connectdb.php";
+  $today = gmdate('Y-m-d');
 
   try {
     if(isset($_POST["newChapter"])){
       //Inserts to database
-      
-      //Creates file
-      $filename = "./chapters/";
-      // $file = fopen();
+      $query = "INSERT INTO chapter (title, storyID, summary) VALUES ('{$_POST["title"]}', $_POST[storyID], '{$_POST["summary"]}')";
+
+      $result = mysqli_query($con, $query);
+
+      if ($result){
+        //Searches the chapter ID
+        $search = "SELECT chapterID FROM chapter WHERE title='{$_POST["title"]}' AND storyID = $_POST[storyID]";
+        $res = mysqli_query($con, $search);
+
+        $id = mysqli_fetch_array($res)[0];
+
+        if ($id){        //Creates file
+          $filename = "./chapters/$id.txt";
+          // $file = fopen();
+          
+          $result = true;
+        }
+        else{
+          $result = false;
+        }
+      }
+      else {
+        $result = false;
+      }
     }
-    $result = true;
   } catch (\Throwable $th) {
     //throw $th;
     $result = $th->getMessage();
