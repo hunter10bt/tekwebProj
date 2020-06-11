112fe776b485524729cd295d124d9a0252c6e83b
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-11 17:47:07 +0700

    initialized new chapter function

--- addChapter.php
+++ addChapter.php
@@ -2,6 +2,20 @@
   session_start();
   include "connectdb.php";
 
+  try {
+    if(isset($_POST["newChapter"])){
+      //Inserts to database
+      
+      //Creates file
+      $filename = "./chapters/";
+      // $file = fopen();
+    }
+    $result = true;
+  } catch (\Throwable $th) {
+    //throw $th;
+    $result = $th->getMessage();
+  }
 
+  echo json_encode($result);
   exit();
 ?>
