264f2e703a1dc7a5e845798bdb06cb29968f01fe
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-22 15:04:22 +0700

    added exception handling to load discussion list module

--- loadDiscussionList.php
+++ loadDiscussionList.php
@@ -2,35 +2,42 @@
   include "connectdb.php";
   $result = "";
   
-  if(isset($_POST["updateDiscussionList"])){
-    $query = "SELECT discussionID,title,user FROM discussion WHERE readable = 1";
-    if(isset($_POST["franchiseID"])){
-      $query .= " AND franchiseID = '{$_POST["franchiseID"]}'";
-    }
-    elseif (isset($_POST["storyID"])) {
-      $query .= " AND storyID = $_POST[storyID]";
-    }
-    $discussionRes = mysqli_query($con, $query);
-    if ($discussionRes) {
-      $count = 0;
-      while($row = mysqli_fetch_array($discussionRes)){
-        $result .= "
-        <a href='forum.php?id=$row[0]' class='list-group-item list-group-item-action'>
-          <div class='d-flex w-100 justify-content-between'>
-            <h4 class='mb-1'>$row[1]</h5>
-            <!--<small>3 days ago</small>-->
-          </div>
-          <small>By $row[2]</small>
-        </a>";
-        $count += 1;
+  try {
+    if(isset($_POST["updateDiscussionList"])){
+      $query = "SELECT discussionID,title,user FROM discussion WHERE readable = 1";
+      if(isset($_POST["franchiseID"])){
+        $query .= " AND franchiseID = '{$_POST["franchiseID"]}'";
       }
-      if ($count < 1) {
-        $result = "There are currently no discussions";
+      elseif (isset($_POST["storyID"])) {
+        $query .= " AND storyID = $_POST[storyID]";
+      }
+      $discussionRes = mysqli_query($con, $query);
+      if ($discussionRes) {
+        $count = 0;
+        while($row = mysqli_fetch_array($discussionRes)){
+          $result .= "
+          <a href='forum.php?id=$row[0]' class='list-group-item list-group-item-action'>
+            <div class='d-flex w-100 justify-content-between'>
+              <h4 class='mb-1'>$row[1]</h5>
+              <!--<small>3 days ago</small>-->
+            </div>
+            <small>By $row[2]</small>
+          </a>";
+          $count += 1;
+        }
+        if ($count < 1) {
+          $result = "There are currently no discussions";
+        }
+      }
+      else {
+        $result = "Cannot find list of discussions.";
       }
     }
     else {
-      $result = "Cannot find list of discussions.";
+      $result = "No signal received.";
     }
+  } catch (Throwable $th) {
+    $result = $th -> getMessage();
   }
 
   echo $result;
