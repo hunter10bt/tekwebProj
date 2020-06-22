0efdb48e92c25d318d85e39cb57502c1e1a3f581
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-22 16:02:20 +0700

    improved error handling in add discussion module

--- addDiscussion.php
+++ addDiscussion.php
@@ -2,23 +2,32 @@
   session_start();
   include "connectdb.php";
   $today = gmdate('Y-m-d');
+  $result = "";
 
-  if(isset($_POST["newDiscussion"]) && isset($_SESSION["uname"])){
-    if (isset($_POST["franchiseID"])){
-      $query = "INSERT INTO `discussion` (dateCreated, title, user, franchiseID, content) VALUES ('$today', '{$_POST["title"]}', '{$_SESSION["uname"]}', '{$_POST["franchiseID"]}', '{$_POST["details"]}')";
-
-      $result = mysqli_query($con, $query);
-      echo json_encode($result);
+  try {
+    if(isset($_POST["newDiscussion"]) && isset($_SESSION["uname"])){
+      $query = "";
+      if (isset($_POST["franchiseID"])){
+        $query = "INSERT INTO `discussion` (dateCreated, title, user, franchiseID, content) VALUES ('$today', '{$_POST["title"]}', '{$_SESSION["uname"]}', '{$_POST["franchiseID"]}', '{$_POST["details"]}')";
+      }
+      elseif (isset($_POST["storyID"])){
+        $query = "INSERT INTO `discussion` (dateCreated, title, user, storyID, content) VALUES ('$today', '{$_POST["title"]}', '{$_SESSION["uname"]}', '{$_POST["storyID"]}', '{$_POST["details"]}')";
+      }
+      $check = mysqli_query($con, $query);
+      if ($check) {
+        $result = "Successfully added discussion.";
+      } else {
+        $result = "Adding discussion failed.";
+      }
     }
-    elseif (isset($_POST["storyID"])){
-      $query = "INSERT INTO `discussion` (dateCreated, title, user, storyID, content) VALUES ('$today', '{$_POST["title"]}', '{$_SESSION["uname"]}', '{$_POST["storyID"]}', '{$_POST["details"]}')";
-
-      $result = mysqli_query($con, $query);
-      echo json_encode($result);
+    else {
+      if(!isset($_POST["newDiscussion"])) $result .= " No signal received.";
+      if(!isset($_SESSION["uname"])) $result .= " You are not logged in. You cannot add discussion.";
     }
+  } catch (Throwable $th) {
+    $result = $th -> getMessage();
   }
-  else {
-    echo json_encode("Unable to add discussion");
-  }
+
+  echo $result;
   exit();
 ?>
