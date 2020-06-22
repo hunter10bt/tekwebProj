9416689993d1ecf62eb4084629d648d9984f2a8f
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-22 14:59:18 +0700

    converged discussion list loading function due to duplication

--- loadDiscussionList.php
+++ loadDiscussionList.php
@@ -1,47 +1,37 @@
 <?php
   include "connectdb.php";
+  $result = "";
   
   if(isset($_POST["updateDiscussionList"])){
     if(isset($_POST["franchiseID"])){
       $query = "SELECT discussionID,title,user FROM discussion WHERE readable = 1 AND franchiseID = '{$_POST["franchiseID"]}'";
-      $discussionRes = mysqli_query($con, $query);
-
-      if ($discussionRes) {
-        while($row = mysqli_fetch_array($discussionRes)){
-          echo "
-          <a href='forum.php?id=$row[0]' class='list-group-item list-group-item-action'>
-            <div class='d-flex w-100 justify-content-between'>
-              <h4 class='mb-1'>$row[1]</h5>
-              <!--<small>3 days ago</small>-->
-            </div>
-            <small>By $row[2]</small>
-          </a>";
-        }
-      }
-      else {
-        echo "No discussions.";
-      }
     }
     elseif (isset($_POST["storyID"])) {
       $query = "SELECT discussionID,title,user FROM discussion WHERE readable = 1 AND storyID = $_POST[storyID]";
-      $discussionRes = mysqli_query($con, $query);
-
-      if ($discussionRes) {
-        while($row = mysqli_fetch_array($discussionRes)){
-          echo "
-          <a href='forum.php?id=$row[0]' class='list-group-item list-group-item-action'>
-            <div class='d-flex w-100 justify-content-between'>
-              <h4 class='mb-1'>$row[1]</h5>
-            </div>
-            <small>By $row[2]</small>
-          </a>";
-        }
+    }
+    $discussionRes = mysqli_query($con, $query);
+    if ($discussionRes) {
+      $count = 0;
+      while($row = mysqli_fetch_array($discussionRes)){
+        $result .= "
+        <a href='forum.php?id=$row[0]' class='list-group-item list-group-item-action'>
+          <div class='d-flex w-100 justify-content-between'>
+            <h4 class='mb-1'>$row[1]</h5>
+            <!--<small>3 days ago</small>-->
+          </div>
+          <small>By $row[2]</small>
+        </a>";
+        $count += 1;
       }
-      else {
-        echo "No discussions.";
+      if ($count < 1) {
+        $result = "There are currently no discussions";
       }
     }
-    exit();
+    else {
+      $result = "Cannot find list of discussions.";
+    }
   }
 
+  echo $result;
+  exit();
 ?>
