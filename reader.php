e933085239f53e22d6fbf2a998485eb759e47061
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-22 19:36:22 +0700

    removed search form from navbar

--- reader.php
+++ reader.php
@@ -64,11 +64,10 @@
             echo '</li>';
           }
         ?>
+        <li class='nav-item'>
+          <a class='nav-link' href='about.php'>About</a>
+        </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
     </div>
   </nav>
   <div class="container-fluid"id="container">
--- signin.php
+++ signin.php
@@ -34,11 +34,10 @@
         <li class="nav-item">
           <a class="nav-link" href="signup.php?prevPage=<?php if (isset($_GET["prevPage"])) echo $_GET["prevPage"] ; else echo "index.php"; ?>">Sign Up<span class="sr-only">(current)</span></a>
         </li>
+        <li class='nav-item'>
+          <a class='nav-link' href='about.php'>About</a>
+        </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
     </div>
   </nav>
   <div class="container-fluid"id="container">
