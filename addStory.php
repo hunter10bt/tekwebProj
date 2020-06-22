ededd622cdc51fe44caed1b3278ee7829ea800ad
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-22 16:27:45 +0700

    improved error handling in add story module

--- addStory.php
+++ addStory.php
@@ -1,27 +1,35 @@
 <?php
   session_start();
   include "connectdb.php";
-  $result = false;
+  $result = "";
   $today = gmdate('Y-m-d');
   try {
-    //code...
     if(isset($_POST["newStory"]) && isset($_SESSION["uname"])){
       $query = "INSERT INTO `story` (dateAdded, title, author, summary) VALUES ('$today', '{$_POST["title"]}', '{$_SESSION["uname"]}','{$_POST["details"]}')";
 
-      $result = mysqli_query($con, $query);
-      if (isset($_POST["franchiseID"])){
-        $idsearch = mysqli_query($con, "SELECT storyID FROM story WHERE title = '{$_POST["title"]}'");
-
-        $id = mysqli_fetch_array($idsearch)[0];
-        $tagAdd = mysqli_query($con, "INSERT INTO tagdetails VALUES ('{$_POST["franchiseID"]}','$id')");
-        $result .= ": Tag added";
+      $add = mysqli_query($con, $query);
+      if ($add) {
+        $result = "Story";
+        if (isset($_POST["franchiseID"])){
+          $idsearch = mysqli_query($con, "SELECT storyID FROM story WHERE title = '{$_POST["title"]}'");
+  
+          $id = mysqli_fetch_array($idsearch)[0];
+          $tagAdd = mysqli_query($con, "INSERT INTO tagdetails VALUES ('{$_POST["franchiseID"]}','$id')");
+          $result .= " and tag";
+        }
+        $result .= " succesfully added.";
+      } else {
+        $result = "Failed to add story.";
       }
     }
-  } catch (\Throwable $th) {
-    //throw $th;
+    else {
+      if (!isset($_POST["newStory"])) $result .= " No signal received.";
+      if (!isset($_SESSION["uname"])) $result .= " Unable to add story because you are not logged in.";
+    }
+  } catch (Throwable $th) {
     $result = $th->getMessage();
   }
 
-  echo json_encode($result);
+  echo $result;
   exit();
 ?>
