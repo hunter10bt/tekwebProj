d63d4c8493e242cbd85b856a21db1a1544413ab3
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-22 14:51:06 +0700

    added exception handling in "load chapter list" module

--- loadChapterList.php
+++ loadChapterList.php
@@ -1,33 +1,39 @@
 <?php
   include "connectdb.php";
   session_start();
+  $result = "";
 
-  if(isset($_POST["loadChapters"])){
-    
-    $query = "SELECT chapterID,title,summary FROM chapter WHERE readable = 1 AND storyID = $_POST[storyID]";
-    $chapterRes = mysqli_query($con, $query);
-    $result = "";
+  try {
+    if(isset($_POST["loadChapters"])){
+      $query = "SELECT chapterID,title,summary FROM chapter WHERE readable = 1 AND storyID = $_POST[storyID]";
+      $chapterRes = mysqli_query($con, $query);
+      $result = "";
 
-    if ($chapterRes){
-      $count = 0;
-      while ($row = mysqli_fetch_array(($chapterRes))) {
-        $result.= "
-        <a href='reader.php?id=$row[0]' class='list-group-item list-group-item-action'>
-          <div class='d-flex w-100 justify-content-between'>
-            <h4 class='mb-1'>$row[1]</h5>
-          </div>
-          <p class='mb-1'>$row[2]</p>
-        </a>";
-        $count +=1;
+      if ($chapterRes){
+        $count = 0;
+        while ($row = mysqli_fetch_array(($chapterRes))) {
+          $result.= "
+          <a href='reader.php?id=$row[0]' class='list-group-item list-group-item-action'>
+            <div class='d-flex w-100 justify-content-between'>
+              <h4 class='mb-1'>$row[1]</h5>
+            </div>
+            <p class='mb-1'>$row[2]</p>
+          </a>";
+          $count += 1;
+        }
+        if($count < 1){
+          $result .= "There are currently no chapters.";
+        }
       }
-      if($count < 1){
-        echo "No chapters.";
+      else {
+        $result .= "Cannot find chapters.";
       }
-      
     }
     else {
-      $result.= "There are currently no chapters.";
+      $result = "No signal received.";
     }
+  } catch (Throwable $th) {
+    $result = $th -> getMessage();
   }
 
   echo $result;
