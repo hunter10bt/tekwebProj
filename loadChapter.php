3a3a32df831be7a3854e26b5e35a9c2910e6ac72
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-22 16:49:49 +0700

    removed unnecesarry characters from loadChapter file

--- loadChapter.php
+++ loadChapter.php
@@ -4,18 +4,14 @@
   $result = "";
 
   try {
-    //code...
     if (isset($_POST["loadChapter"]) && isset($_POST["chapterID"])) {
-      # code...
       $filename = "./chapters/$_POST[chapterID].json";
       $json = file_get_contents($filename);
 
       if ($json) {
-        # code...
         $array = json_decode($json);
 
         foreach ($array as $paragraph) {
-          # code...
           $result .= "<div>$paragraph</div>";
         }
       }
@@ -26,9 +22,7 @@
     else {
       $result = false;
     }
-    
-  } catch (\Throwable $th) {
-    //throw $th;
+  } catch (Throwable $th) {
     $result = $th->getMessage();
   }
 
