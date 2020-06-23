cdb740b9743b0ea8885b517a2447b065bbf9595f
hunter10bt <57027372+hunter10bt@users.noreply.github.com>
2020-06-23 17:15:12 +0700

    deleted admin folder

--- admin/adminOut.php
+++
@@ -1,9 +0,0 @@
-<?php
-  session_start();
-  session_destroy();
-
-  if(isset($_GET["prevPage"]))
-    header("location: ".$_GET["prevPage"]);
-  else
-    header("location: admin.php");
-?>
--- admin/connectdbAdmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $host = 'localhost';
-  $user = 'root';
-  $pass = '';
-  $db = 'readhere';
-  $con = mysqli_connect(
-    $host,$user,$pass,$db
-  );
-?>
